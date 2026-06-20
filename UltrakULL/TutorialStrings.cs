2d167c412df5e8ab6678092bf7747c73ded230ff
Quark <quarkumgames@gmail.com>
2026-06-20 22:48:15 +0700

    moved some colors and removed quotes of binds

--- UltrakULL/TutorialStrings.cs
+++ UltrakULL/TutorialStrings.cs
@@ -40,15 +40,15 @@ namespace UltrakULL
 
             if (fullMessage.Contains("PUNCH"))
             {
-                return (LanguageManager.CurrentLanguage.tutorial.tutorial_punch1 + " '<color=orange>" + input + "</color>' " + LanguageManager.CurrentLanguage.tutorial.tutorial_punch2);
+                return (LanguageManager.CurrentLanguage.tutorial.tutorial_punch1 + "<color=orange>" + input + "</color>" + LanguageManager.CurrentLanguage.tutorial.tutorial_punch2);
             }
             else if (fullMessage.Contains("SLIDE"))
             {
-                return (LanguageManager.CurrentLanguage.tutorial.tutorial_slide1 + " '<color=orange>" + input + "</color>' " + LanguageManager.CurrentLanguage.tutorial.tutorial_slide2);
+                return (LanguageManager.CurrentLanguage.tutorial.tutorial_slide1 + "<color=orange>" + input + "</color>" + LanguageManager.CurrentLanguage.tutorial.tutorial_slide2);
             }
             else if (fullMessage.Contains("DASH"))
             {
-                return (LanguageManager.CurrentLanguage.tutorial.tutorial_dash1 + " '<color=#00DFFF>" + input + "</color>' " + LanguageManager.CurrentLanguage.tutorial.tutorial_dash2 + "\n" + LanguageManager.CurrentLanguage.tutorial.tutorial_dash3);
+                return (LanguageManager.CurrentLanguage.tutorial.tutorial_dash1 + "<color=#00DFFF>" + input + "</color>" + LanguageManager.CurrentLanguage.tutorial.tutorial_dash2 + "\n" + LanguageManager.CurrentLanguage.tutorial.tutorial_dash3);
             }
             else if (fullMessage.Contains("HEALTH"))
             {
@@ -60,7 +60,7 @@ namespace UltrakULL
             }
             else if (fullMessage.Contains("SHOCKWAVE"))
             {
-                return (LanguageManager.CurrentLanguage.tutorial.tutorial_shockwave1 + " '<color=orange>" + input + "</color>' " + LanguageManager.CurrentLanguage.tutorial.tutorial_shockwave2 + "\n" + LanguageManager.CurrentLanguage.tutorial.tutorial_shockwave3);
+                return (LanguageManager.CurrentLanguage.tutorial.tutorial_shockwave1 + "<color=orange>" + input + "</color>" + LanguageManager.CurrentLanguage.tutorial.tutorial_shockwave2 + "\n" + LanguageManager.CurrentLanguage.tutorial.tutorial_shockwave3);
             }
             else if (fullMessage.Contains("ORBS"))
             {
